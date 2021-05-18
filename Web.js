971de9e410b9746d18846a64ce7a6f8ae7bb5032
Chaz Logan <chazaqlogan@gmail.com>
2021-05-18 08:02:17 -0400

    How does the web work

---
+++ Web.js
@@ -0,0 +1,41 @@
+What is HTTP ?
+    Hyper Text Transfer Protocol determines how clients get data from or send data to a ServiceWorkerRegistration
+
+
+What is  URL ?
+    Uniform Resource Locator, it is the internet address
+
+What is DNS ?
+    Domain Name System - it is a system that takes human eradable URLS and converts them into IP addresses
+
+What is a query String ??
+
+    The query string allows you to pass key - value pairs into the URL, in the format ?
+        key1 = value1 & key2=value2...MediaKeySystemAccess.apply
+
+List two HTTP verbs and their use CSSNamespaceRule.apply
+
+GET - get some data from the server(most pages, search forms)
+POST - send some data to the server(pages that change data on server)
+
+What is an HTTP Request ?
+
+    An HTTP request is a request from a client to a server which follows the HTTP Protocol
+example a request for HTML from news.cnn.com
+
+What is an HTTP response ?
+    An HTTP response is a response from a server to a client which follows the HTTP protocol(sending back HTML, CSS, JS, etc)...Protocol
+
+What is an HTTP header ? Give a couple examples of request and response headers you have screen.availHeight
+Headers provide additional information about the request or the response...
+Request headers: HTMLOListElement, User - AudioProcessingEvent, Accept, Cookie, Cache - Control
+Response headers: Content - Type, Last - Modified, Set - Cookie, Cache - Control
+
+
+What happens when you type a URL in a browser ?
+
+    1) Your browser "resolves" the name into an IP address using DNS
+2) Your browser makes a request to that IP address, including headers(infor about browser, any previous cookies and other things)
+3) The server sends a response(typcially HTML with a status code(200 if it was successful))
+4) The browser makes a DOM from that HTML, and finds any other resources needed(images, CSS, JavaScript...etc)
+5) The browser makes seperate HTTP requests for those resources and receives response from the server for each
